30332d881e4696d2f6693e85daa2636e42067c79
Miriam Silva <miriam-silva@live.com>
2021-05-11 07:00:10 -0700

    js & css changes

--- app.js
+++ app.js
@@ -2,35 +2,14 @@
 
 const seattle = {
   name: 'seattle',
+  sales:0,
   customerSales: function() {
     console.log(this.sale);
     this.sale = randomCustomer(23,65,6.3) + ' sales per hour';
   }
 }
-const tokyo = {
-  name: 'seattle',
-  customerSales: function() {
-    console.log(this.sale);
-    this.sale = randomCustomer(3,24,1.2) + ' sales per hour';
-  }
-}
-const dubai = {
-  name: 'dubai',
-  customerSales: function() {
-    console.log(this.sale);
-    this.sale = randomCustomer(11,38,3.7) + ' sales per hour';
-  }
-}
-const paris = {
-  name: 'paris',
-  customerSales: function() {
-    console.log(this.sale);
-    this.sale = randomCustomer(20,38,2.3) + ' sales per hour';
-  }
-}
-
 function randomCustomer(min, max, avg) {
-  return (Math.floor(Math.random() * (max - min) + min))*avg;
+  return Math.floor(Math.random() * ((max - min) + min))*avg;
 }
 
 let hoursArray = ['6am ','7am ', '8am ', '9am ','10am ', '11am ', '12pm ','1pm ','2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm '];
@@ -39,9 +18,6 @@ let hoursArrayTwo = hoursArray.map(myFunction);
 function myFunction(){
   seattle.customerSales();
 }
-
-console.log(seattle.customerSales());
-
 const locationDivElem = document.getElementById ('locationSales');
 
 function makeLocationArticle(location){
@@ -57,14 +33,11 @@ function makeLocationArticle(location){
   const ulElem = document.createElement('ul');
   articleElem.appendChild(ulElem);
 
-  for (let i = 0; i< hoursArray.length; i++){
-    const liElem = document.createElement('li');
-    liElem.textContent = hoursArray[i] + location.customerSales[i];
-    ulElem.appendChild(liElem);
-   }
+  for (let i = 0; i< hoursArray.length; i++){ 
+      const liElem = document.createElement('li');
+      liElem.textContent = hoursArray[i] + location.sales[i];
+      ulElem.appendChild(liElem);
+  }
 }
 
 makeLocationArticle(seattle);
-makeLocationArticle(tokyo);
-makeLocationArticle(dubai);
-makeLocationArticle(paris);
